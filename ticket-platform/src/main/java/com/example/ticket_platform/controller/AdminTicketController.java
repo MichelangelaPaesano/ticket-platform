2ac3e157ad5c6f6141729a116c5cd5963a762c06
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-10-17 12:26:29 +0200

    get post e thymeleaf

--- ticket-platform/src/main/java/com/example/ticket_platform/controller/AdminTicketController.java
+++ ticket-platform/src/main/java/com/example/ticket_platform/controller/AdminTicketController.java
@@ -1,15 +1,26 @@
 package com.example.ticket_platform.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.ticket_platform.model.Ticket;
 import com.example.ticket_platform.repository.TicketRepository;
+
+import jakarta.validation.Valid;
+
+
+
 
 
 @Controller
@@ -21,12 +32,80 @@ public class AdminTicketController {
     private TicketRepository repository;
     //per adesso inietto tolo ticket repository perchè credo che debba lavorare solo su quella per ora!!!
 
+    //Questo Get Mapping serve per la lettura della lista dei ticket 
     @GetMapping("/")
     public String index (Model model) {
         List<Ticket> mostraLista = repository.findAll();
-        model.addAttribute("tickets", mostraLista);
-        return "admin/ticket";
+        model.addAttribute("ticket", mostraLista);
+        return "admin/tickets";
     }
+    
+    //questo show è per vedere un singolo ticket!!! 
+    @GetMapping("/show/{id}")
+    public String show (@PathVariable("id") Integer id, Model model) {
+        Optional<Ticket> optionalTicket = repository.findById(id);
+        if (optionalTicket.isPresent()) {
+            model.addAttribute("ticket", optionalTicket.get());
+            return "admin/show";
+        } else {
+            return "redirect:/admin/tickets"; 
+        }
+
+    }
+
+    //Adesso facciamo get e post per la creazione di un nuovo ticket 
+    @GetMapping("/create")
+    public String create (Model model) {
+        model.addAttribute("ticket", new Ticket());
+        return "admin/create";
+    }
+
+    @PostMapping("/create")
+    public String save (@Valid @ModelAttribute("ticket") Ticket formTicket, 
+                        BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        //controlliamo con binding result se ci sono errori 
+        if (bindingResult.hasErrors()) {
+            return "admin/create";
+        }
+        repository.save(formTicket);
+        //e mostriamo un messaggio di salvataggio
+        redirectAttributes.addFlashAttribute("successMessage", "Ticket creato con successo");
+        return "redirect:/admin/tickets";
+    }
+
+
+    //Adesso facciamo get e post per editare e aggiornare!!!
+    @GetMapping("/edit/{id}")
+    public String edit (@PathVariable("id") Integer id, Model model) {
+        Optional<Ticket> optTicket = repository.findById(id);
+        if (optTicket.isPresent()) {
+        Ticket ticket = optTicket.get();
+        model.addAttribute("ticket", ticket);
+        return "admin/edit";
+        } else {
+            return "redirect:/admin/tickets";
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update (@Valid @ModelAttribute ("ticket") Ticket formTicket,
+                        BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "admin/create";
+        }
+        //ci assicuriamo che l'id esista!
+        Optional<Ticket> optTicket = repository.findById(id);
+        if (optTicket.isPresent()) {
+            repository.save(formTicket);
+            redirectAttributes.addFlashAttribute("successMessage", "Ticket modificato con successo");
+           
+        }
+        return "redirect:/admin/tickets";
+        
+    }
+    
+    
+    
     
     
 }
