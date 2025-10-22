208ef4b0a437a7fff7acb8be9b281b0114a3980a
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-10-22 12:00:43 +0200

    security pt2

--- ticket-platform/src/main/java/com/example/ticket_platform/security/SecurityConfiguration.java
+++ ticket-platform/src/main/java/com/example/ticket_platform/security/SecurityConfiguration.java
@@ -1,6 +1,59 @@
 package com.example.ticket_platform.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests()
+                .requestMatchers("/admin/tickets", "/admin/tickets/edit/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/admin/tickets/**").hasRole("ADMIN")
+                .requestMatchers("/admin/tickets/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/default", true)
+                .permitAll()
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+            .and()
+            .csrf().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 }
+
